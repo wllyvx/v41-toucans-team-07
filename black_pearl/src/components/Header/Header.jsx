627d83bca79557b9641b48db313aa90fa43a1a0c
Willy Fajar Ramadhan <willyfr88@gmail.com>
2022-10-23 16:18:00 +0700

    adding header

--- black_pearl/src/components/Header/Header.jsx
+++ black_pearl/src/components/Header/Header.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import "./Header.scss";
+import header_img from "../../assets/hero-square.jpg";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="header-container">
+        <img src={header_img} alt="" className="header-img" />
+        <div className="title-container">
+          <h1 className="title">The Black Pearl</h1>
+          <h2 className="web-desc">
+            A Page dedicated to Fan of Pirates of Caribbean Movie (A FanPage)
+          </h2>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
