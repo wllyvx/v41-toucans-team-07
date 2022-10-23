0b398737db55f86b5db7b73b3486e168e0236c6d
Willy Fajar Ramadhan <willyfr88@gmail.com>
2022-10-23 16:17:38 +0700

    fixing the unused components

--- black_pearl/src/App.js
+++ black_pearl/src/App.js
@@ -1,7 +1,5 @@
-import React from 'react';
-import Header from './components/Header/Header';
-import Navbar from './components/Navbar/Navbar';
-import LandingPage from './pages/LandingPage/LandingPage';
+import React from "react";
+import LandingPage from "./pages/LandingPage/LandingPage";
 
 const App = () => {
   return (
