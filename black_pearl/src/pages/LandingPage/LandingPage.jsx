7e1eeff07b3c9fa375f07f2f1323f20428e6f16b
Willy Fajar Ramadhan <willyfr88@gmail.com>
2022-10-24 05:23:12 +0700

    moving the global styling from sotd to landingpage

--- black_pearl/src/pages/LandingPage/LandingPage.jsx
+++ black_pearl/src/pages/LandingPage/LandingPage.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Header from "../../components/Header/Header";
 import Sotd from "../../components/Sotd/Sotd";
+import MovieSection from "../../components/Movie/MovieSection";
+import "./landingpage.scss";
 
 const LandingPage = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <MovieSection />
       <Sotd /> {/* SOTD = Ship of The Day */}
     </div>
   );
