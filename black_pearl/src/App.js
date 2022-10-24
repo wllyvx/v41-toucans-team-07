56c5fc0174513c2c3de14d405d680d247432de77
Willy Fajar Ramadhan <willyfr88@gmail.com>
2022-10-25 06:40:11 +0700

    moving navbar to app.js

--- black_pearl/src/App.js
+++ black_pearl/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
 import LandingPage from "./pages/LandingPage/LandingPage";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <LandingPage />
       {/* <Header /> */}
     </div>
--- black_pearl/src/pages/LandingPage/LandingPage.jsx
+++ black_pearl/src/pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "../../components/Navbar/Navbar";
 import Header from "../../components/Header/Header";
 import Sotd from "../../components/Sotd/Sotd";
 import MovieSection from "../../components/Movie/MovieSection";
@@ -8,7 +7,6 @@ import "./landingpage.scss";
 const LandingPage = () => {
   return (
     <div>
-      <Navbar />
       <Header />
       <MovieSection />
       <Sotd /> {/* SOTD = Ship of The Day */}
