3561d0ac222b211c28e4f38fba7cf8d34285cbc7
Willy Fajar Ramadhan <willyfr88@gmail.com>
2022-10-25 07:08:01 +0700

    moving the global styling to index.scss

--- black_pearl/src/App.js
+++ black_pearl/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./index.scss";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
+import Movie from "./pages/Movie/Movie";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Movie />} />
+      </Routes>
       {/* <Header /> */}
     </div>
   );
---
+++ black_pearl/src/pages/Movie/Movie.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Movie() {
+  return (
+    <div>
+      <h1>Movie</h1>
+      <p>this is movie page</p>
+    </div>
+  );
+}
+
+export default Movie;
